b6bfb0df7289de695e914dd21613fe3e6a4deb39
Karim Wael <devkarim@hotmail.com>
2024-12-05 04:46:53 +0200

    feat: add sample tab views

---
+++ src/main/java/com/bank/views/AddEntityPanel.java
@@ -0,0 +1,11 @@
+package com.bank.views;
+
+import com.bank.ui.Label;
+
+import javax.swing.*;
+
+public class AddEntityPanel extends JPanel {
+    public AddEntityPanel(){
+        this.add(new Label("Add Entity"));
+    }
+}
---
+++ src/main/java/com/bank/views/ViewEntityPanel.java
@@ -0,0 +1,11 @@
+package com.bank.views;
+
+import com.bank.ui.Label;
+
+import javax.swing.*;
+
+public class ViewEntityPanel extends JPanel {
+    public ViewEntityPanel(){
+        this.add(new Label("View Entity"));
+    }
+}
