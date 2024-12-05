a5f38b9926c2244bb8d023550083e72bb39268b3
Karim Wael <devkarim@hotmail.com>
2024-12-05 05:16:42 +0200

    refactor: set main frame to appear centered of the user's screen

--- src/main/java/com/bank/views/Application.java
+++ src/main/java/com/bank/views/Application.java
@@ -11,7 +11,8 @@ import javax.swing.*;
  * <p>
  * This class extends {@link JFrame} and serves as the primary container
  * for the graphical user interface (GUI) of the application. It sets up
- * the main window with a fixed size, title, background color, and a tabbed panel
+ * the main window with a fixed size, title, background color, and a tabbed
+ * panel
  * for navigating between different application features.
  * </p>
  */
@@ -21,13 +22,13 @@ public class Application extends JFrame {
      * <p>
      * Initializes the main window with:
      * <ul>
-     *     <li>Fixed dimensions of 1280x768 pixels</li>
-     *     <li>A non-resizable frame</li>
-     *     <li>A predefined background color from {@link Colors}</li>
-     *     <li>Exit-on-close behavior</li>
-     *     <li>A title of "BANK"</li>
-     *     <li>A {@link TabPanel} for displaying tabs, initialized with
-     *         constants from {@link Constants#tabs}</li>
+     * <li>Fixed dimensions of 1280x768 pixels</li>
+     * <li>A non-resizable frame</li>
+     * <li>A predefined background color from {@link Colors}</li>
+     * <li>Exit-on-close behavior</li>
+     * <li>A title of "BANK"</li>
+     * <li>A {@link TabPanel} for displaying tabs, initialized with
+     * constants from {@link Constants#tabs}</li>
      * </ul>
      * Finally, the window is set to be visible.
      * </p>
@@ -39,6 +40,7 @@ public class Application extends JFrame {
         this.setBackground(Colors.BACKGROUND_COLOR);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setTitle("BANK");
+        this.setLocationRelativeTo(null); // Appear centered of the screen
 
         this.add(new TabPanel(Constants.tabs));
 
