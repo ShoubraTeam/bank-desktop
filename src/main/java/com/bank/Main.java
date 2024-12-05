aee2b9728a601f27b7c9265a2ba24b7ff6de1c57
Karim Wael <devkarim@hotmail.com>
2024-12-05 04:48:41 +0200

    refactor: call invokeLater with the application method reference passed
    Calling invokeLater ensures GUI-related code runs on EDT.

--- src/main/java/com/bank/Main.java
+++ src/main/java/com/bank/Main.java
@@ -1,7 +1,12 @@
 package com.bank;
 
+import com.bank.views.Application;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Ensure GUI-related code runs on Event Dispatch Thread (EDT) which is the
+        // thread responsible for managing updates to the graphical user interface
+        // in Swing applications.
+        javax.swing.SwingUtilities.invokeLater(Application::new);
     }
 }
