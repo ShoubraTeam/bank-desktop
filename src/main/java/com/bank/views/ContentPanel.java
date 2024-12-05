09620dbf8b746eaeed064d2d8d8e3e59e0660b91
Karim Wael <devkarim@hotmail.com>
2024-12-05 04:44:49 +0200

    feat: add ContentPanel responsible for showing tab's view

---
+++ src/main/java/com/bank/views/ContentPanel.java
@@ -0,0 +1,10 @@
+package com.bank.views;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPanel extends JPanel {
+    public ContentPanel() {
+        this.setLayout(new CardLayout());
+    }
+}
