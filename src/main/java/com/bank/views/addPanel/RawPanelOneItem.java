8862c850ee1626d5fbd3b8928df4b4108a1f603a
fady <fadyyoussef4444@gamil.com>
2024-12-06 14:13:20 +0200

    create RawPanelOneItem for adding one item on the right panel

--- src/main/java/com/bank/views/addPanel/RawPanelOneItem.java
+++ src/main/java/com/bank/views/addPanel/RawPanelOneItem.java
@@ -3,15 +3,12 @@ package com.bank.views.addPanel;
 import javax.swing.*;
 import java.awt.*;
 
-public class RawPanel<T> extends JPanel {
+public class RawPanelOneItem<T> extends JPanel {
 
-    public RawPanel(T object) {
-
-        this.setPreferredSize(new Dimension(900, 50));
+    public RawPanelOneItem(T object) {
+        this.setPreferredSize(new Dimension(900, 40));
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
         this.add((Component) object);
-
-
     }
 
 }
