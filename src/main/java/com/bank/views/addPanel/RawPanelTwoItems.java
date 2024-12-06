d3446a8cda1941c848f4852691f9b4468807cbde
fady <fadyyoussef4444@gamil.com>
2024-12-06 18:10:22 +0200

    refactor: T extends component class

--- src/main/java/com/bank/views/addPanel/RawPanelTwoItems.java
+++ src/main/java/com/bank/views/addPanel/RawPanelTwoItems.java
@@ -3,13 +3,13 @@ package com.bank.views.addPanel;
 import javax.swing.*;
 import java.awt.*;
 
-public class RawPanelTwoItems<T1, T2> extends JPanel {
+public class RawPanelTwoItems<T1 extends Component, T2 extends Component> extends JPanel {
 
     public RawPanelTwoItems(T1 object1, T2 object2) {
         this.setPreferredSize(new Dimension(900, 80));
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
-        this.add((Component) object1);
-        this.add((Component) object2);
+        this.add(object1);
+        this.add(object2);
     }
 
 }
