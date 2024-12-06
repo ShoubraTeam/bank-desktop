a34b536367f735df52b70ef823fab471b1d50d7f
fady <fadyyoussef4444@gamil.com>
2024-12-06 14:15:32 +0200

    create RawPanelTwoItem for adding Two items together on the right panel (Text Field with its label or Combo Box with its label)

--- src/main/java/com/bank/views/addPanel/RawPanelTwoItems.java
+++ src/main/java/com/bank/views/addPanel/RawPanelTwoItems.java
@@ -1,2 +1,15 @@
-package com.bank.views.addPanel;public class RawPanelTwoItems {
+package com.bank.views.addPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RawPanelTwoItems<T1, T2> extends JPanel {
+
+    public RawPanelTwoItems(T1 object1, T2 object2) {
+        this.setPreferredSize(new Dimension(900, 80));
+        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        this.add((Component) object1);
+        this.add((Component) object2);
+    }
+
 }
