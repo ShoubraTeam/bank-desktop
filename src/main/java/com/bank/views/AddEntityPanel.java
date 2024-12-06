f7c853a6c0efbe449349c13c48f0fd7f8e93b2cd
fady <fadyyoussef4444@gamil.com>
2024-12-06 14:20:16 +0200

    add raw panel for the header, another one for the combo box, and another one for the button in addEntityPanel

--- src/main/java/com/bank/views/AddEntityPanel.java
+++ src/main/java/com/bank/views/AddEntityPanel.java
@@ -1,8 +1,16 @@
 package com.bank.views;
 
+import com.bank.ui.Button;
+import com.bank.ui.ComboBox;
 import com.bank.ui.Header;
+import com.bank.ui.Label;
+import com.bank.views.addPanel.RawPanelOneItem;
+import com.bank.views.addPanel.RawPanelTwoItems;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A panel that represents the "Add Entity" page of the application.
@@ -13,7 +21,35 @@ import javax.swing.*;
  * </p>
  */
 public class AddEntityPanel extends JPanel {
+    String[] list = {"ATM", "Customer"};
+    ComboBox comboBox;
+
     public AddEntityPanel() {
-        this.add(new Header("Add Entity"));
+        comboBox = new ComboBox(list);
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 20));
+        this.add(new RawPanelOneItem<Header>(new Header("Add Entity")));
+        this.add(new RawPanelTwoItems<Label,ComboBox>(new Label("Entity Type"),new ComboBox(list)));
+        this.add(new RawPanelOneItem<JButton>(new Button("OK")));
+        /*
+        * add scroll panel to add on it the rest of the fields
+        * */
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String itemSelected = comboBox.getSelectedItem().toString();
+                if (itemSelected.equals("ATM")) {
+                    System.out.println("ATM");
+                    /*
+                    * function to add the whole attributes of ATM Entity
+                    * */
+                } else if (itemSelected.equals("Customer")) {
+                    System.out.println("Customer");
+                    /*
+                     * function to add the whole attributes of Customer Entity
+                     * */
+                }
+            }
+        });
     }
+
 }
