de27fc9b5ee1b2279bd49c5e749217f92650131f
fady <fadyyoussef4444@gamil.com>
2024-12-06 18:02:03 +0200

    refactor: remove unused libirary

--- src/main/java/com/bank/ui/ComboBox.java
+++ src/main/java/com/bank/ui/ComboBox.java
@@ -3,17 +3,13 @@ package com.bank.ui;
 import com.bank.config.Colors;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.geom.RoundRectangle2D;
-
 public class ComboBox extends JComboBox {
     public ComboBox(String[] list) {
         super(list);
         this.setPreferredSize(new Dimension(890,50));
         this.setBackground(Colors.SECONDARY_COLOR);
         this.setRequestFocusEnabled(false);
-
         this.setFont(new Font("Arial", Font.BOLD, 18));
     }
 
