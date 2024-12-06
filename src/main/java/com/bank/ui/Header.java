26c647c3bbe8cd30a0c4349e4d63c4c08426d874
fady <fadyyoussef4444@gamil.com>
2024-12-06 14:10:04 +0200

    extends from JLabel instead of Label

--- src/main/java/com/bank/ui/Header.java
+++ src/main/java/com/bank/ui/Header.java
@@ -7,7 +7,7 @@ import java.awt.*;
  * The Header class extends {@link Label} to create a custom label styled as a header.
  * It provides constructors to set the text, alignment, and font styling for a header.
  */
-public class Header extends Label {
+public class Header extends JLabel {
     public Header(String text) {
         this(text, SwingConstants.LEFT);
     }
