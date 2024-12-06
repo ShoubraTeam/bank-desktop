ffa412473c372a46a3b54c84e56567871587bafa
fady <fadyyoussef4444@gamil.com>
2024-12-06 14:08:41 +0200

    create TextField class used in the right panel

--- src/main/java/com/bank/ui/TextField.java
+++ src/main/java/com/bank/ui/TextField.java
@@ -1,2 +1,14 @@
-package com.bank.ui;public class textField {
+package com.bank.ui;
+
+import javax.swing.*;
+
+public class TextField extends JTextField {
+    public TextField(String hiddenText)
+    {
+        this.setSize(695,64);
+        this.setText(hiddenText);
+        this.setVisible(true);
+    }
+
+
 }
