dfd3b0bfd486b1610b1f42527da7002802f4e337
Karim Wael <devkarim@hotmail.com>
2024-12-07 03:06:11 +0200

    refactor: add border to text field

--- src/main/java/com/bank/ui/TextField.java
+++ src/main/java/com/bank/ui/TextField.java
@@ -1,12 +1,15 @@
 package com.bank.ui;
 
+import com.bank.config.Colors;
+
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 import java.awt.*;
 
 public class TextField extends JTextField {
     public TextField() {
-        this.setPreferredSize(new Dimension(890, 50));
+        this.setBorder(new MatteBorder(2, 2, 2, 2, Colors.BORDER_COLOR)); // add padding
+        this.setPreferredSize(new Dimension(0, 50));
+        this.setFont(new Font("Arial", Font.BOLD, 18));
     }
-
-
 }
