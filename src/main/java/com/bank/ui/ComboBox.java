808ad51a73d46669f13de19f8cf3f2f77513da10
Karim Wael <devkarim@hotmail.com>
2024-12-07 03:06:00 +0200

    refactor: add padding to combobox

--- src/main/java/com/bank/ui/ComboBox.java
+++ src/main/java/com/bank/ui/ComboBox.java
@@ -1,16 +1,18 @@
 package com.bank.ui;
 
 import com.bank.config.Colors;
+import com.bank.utils.Helpers;
 
 import javax.swing.*;
 import java.awt.*;
-public class ComboBox extends JComboBox {
+
+public class ComboBox extends JComboBox<String> {
     public ComboBox(String[] list) {
         super(list);
-        this.setPreferredSize(new Dimension(890,50));
+        this.setPreferredSize(new Dimension(0, 50));
         this.setBackground(Colors.SECONDARY_COLOR);
         this.setRequestFocusEnabled(false);
         this.setFont(new Font("Arial", Font.BOLD, 18));
+        this.setRenderer(Helpers.addPaddingToComboBox());
     }
-
 }
