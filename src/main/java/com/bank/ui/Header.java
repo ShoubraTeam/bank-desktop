d24aa677daf33f807668c23eff693631521d3a43
Karim Wael <devkarim@hotmail.com>
2024-12-07 03:02:50 +0200

    refactor: make header inherit from label

--- src/main/java/com/bank/ui/Header.java
+++ src/main/java/com/bank/ui/Header.java
@@ -7,13 +7,13 @@ import java.awt.*;
  * The Header class extends {@link Label} to create a custom label styled as a header.
  * It provides constructors to set the text, alignment, and font styling for a header.
  */
-public class Header extends JLabel {
+public class Header extends Label {
     public Header(String text) {
         this(text, SwingConstants.LEFT);
     }
 
     public Header(String text, int horizontalAlignment) {
-        this.setText(text);
+        super(text);
         this.setHorizontalAlignment(horizontalAlignment);
         this.setFont(new Font("Arial", Font.BOLD, 24));
     }
