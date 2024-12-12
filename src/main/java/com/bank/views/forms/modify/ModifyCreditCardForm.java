0e69f66223135d3ad9eea4b8c7295cbe633f13b6
fady <fadyyoussef4444@gamil.com>
2024-12-13 01:33:11 +0200

    feat: add the forms for each option in the dynamic form for the modify panel

---
+++ src/main/java/com/bank/views/forms/modify/ModifyCreditCardForm.java
@@ -0,0 +1,54 @@
+package com.bank.views.forms.modify;
+
+import com.bank.config.Colors;
+import com.bank.config.EntityConstants;
+import com.bank.ui.Button;
+import com.bank.utils.Helpers;
+import com.bank.utils.LinkedListPair;
+import com.bank.views.forms.Form;
+import com.bank.views.forms.RowPanelOneItem;
+import com.bank.views.forms.RowPanelThreeItems;
+import com.bank.views.forms.RowPanelTwoItems;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ModifyCreditCardForm extends Form {
+    public ModifyCreditCardForm() {
+        Button submitBtn = new Button("Submit");
+        JPanel formPanel = new JPanel();
+        formPanel.setLayout(new GridLayout(10, 1, 12, 12));
+        formPanel.setBorder(new EmptyBorder(18, 18, 18, 18));
+
+        this.setLayout(new BorderLayout(12, 12));
+        for (RowPanelTwoItems row : EntityConstants.CREDIT_CARD_MODIFY_ATTRIBUTES) {
+            formPanel.add(row);
+        }
+
+        JScrollPane scrollPane = new JScrollPane(formPanel);
+        scrollPane.setPreferredSize(new Dimension(500, 600));
+        scrollPane.setBorder(new MatteBorder(3, 3, 3, 3, Colors.SECONDARY_COLOR));
+        this.add(scrollPane, BorderLayout.CENTER);
+
+        submitBtn.addActionListener(submit());
+        this.add(new RowPanelOneItem<>(submitBtn), BorderLayout.SOUTH);
+    }
+    public ActionListener submit() {
+        return (e) -> {
+            LinkedListPair linkedListPair = Helpers.getValuesFromInputsModify((RowPanelThreeItems<?, ?, ?>[]) EntityConstants.CREDIT_CARD_MODIFY_ATTRIBUTES);
+            // ADD YOUR CODE HERE
+            //first list contain the values that we put in update sentence
+            System.out.println(Arrays.toString(linkedListPair.getFirstList().keySet().toArray()));
+            System.out.println(Arrays.toString(linkedListPair.getFirstList().values().toArray()));
+            System.out.println();
+            //second list contain the values that we put in the where condition
+            System.out.println(Arrays.toString(linkedListPair.getSecondList().keySet().toArray()));
+            System.out.println(Arrays.toString(linkedListPair.getSecondList().values().toArray()));
+        };
+    }
+}
