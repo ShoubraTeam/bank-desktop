18b8ddfb4dbddfbb11872601aa0559402d0b2a8c
fady <fadyyoussef4444@gamil.com>
2024-12-13 01:28:06 +0200

    feat: add function to return the group of radio button if exist in the row

--- src/main/java/com/bank/views/forms/RowPanelOneItem.java
+++ src/main/java/com/bank/views/forms/RowPanelOneItem.java
@@ -1,12 +1,24 @@
 package com.bank.views.forms;
 
+import com.bank.ui.RadioButton;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class RowPanelOneItem<T extends Component> extends JPanel {
+    ButtonGroup buttonGroup = new ButtonGroup();
+
     public RowPanelOneItem(T object) {
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
         this.add(object);
+        if (object instanceof RadioButton) {
+            buttonGroup = new ButtonGroup();
+            buttonGroup.add((RadioButton) object);
+        }
+    }
+
+    public ButtonGroup getComponentIfRadioButton() {
+        return buttonGroup;
     }
 
 }
