f6b7a840a1e5ea2d323e49a0ea72a79c2bd8919e
fady <fadyyoussef4444@gamil.com>
2024-12-13 01:23:56 +0200

    feat: create radio button class

---
+++ src/main/java/com/bank/ui/RadioButton.java
@@ -0,0 +1,12 @@
+package com.bank.ui;
+
+import javax.swing.*;
+
+public class RadioButton extends JRadioButton {
+
+    public RadioButton(String Label) {
+        super(Label);
+        this.setFocusPainted(false);
+
+    }
+}
