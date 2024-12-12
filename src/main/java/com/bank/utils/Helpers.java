b7fb063d5076d6354610547ac0571f439955ed27
fady <fadyyoussef4444@gamil.com>
2024-12-13 01:35:15 +0200

    feat: add the function to get the values from the forms in the modify panel

--- src/main/java/com/bank/utils/Helpers.java
+++ src/main/java/com/bank/utils/Helpers.java
@@ -1,10 +1,16 @@
 package com.bank.utils;
 
+import com.bank.config.EntityConstants;
+import com.bank.ui.CheckBoxPanel;
+import com.bank.views.forms.RowPanelOneItem;
+import com.bank.views.forms.RowPanelThreeItems;
 import com.bank.views.forms.RowPanelTwoItems;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
 
 public class Helpers {
     // add padding - got from chatgpt
@@ -20,10 +26,10 @@ public class Helpers {
         };
     }
 
-    public static ArrayList<String> getValuesFromInputs(RowPanelTwoItems<?,?>[] attrs) {
+    public static ArrayList<String> getValuesFromInputs(RowPanelTwoItems<?, ?>[] attrs) {
         ArrayList<String> values = new ArrayList<>();
 
-        for (RowPanelTwoItems<?,?> row : attrs) {
+        for (RowPanelTwoItems<?, ?> row : attrs) {
             String value;
             if (row.getSecondComponent() instanceof JTextField) {
                 value = ((JTextField) row.getSecondComponent()).getText();
@@ -37,4 +43,79 @@ public class Helpers {
 
         return values;
     }
+
+    public static LinkedListPair getValuesFromInputsModify(RowPanelThreeItems<?, ?, ?>[] attrs) {
+        HashMap<String,String> values = new HashMap<String,String>();
+        HashMap<String,String> wherevalues = new HashMap<String,String>();
+
+        for (RowPanelThreeItems<?, ?, ?> row : attrs) {
+            String value;
+            String key;
+            if (row.getThirdComponent() instanceof CheckBoxPanel<?, ?>) {
+
+                Enumeration<AbstractButton> buttons = ((CheckBoxPanel<?, ?>) row.getThirdComponent()).getGroup().getElements();
+                while (buttons.hasMoreElements()) {
+                    JRadioButton button = (JRadioButton) buttons.nextElement();
+                    if (button.isSelected()) {
+                        if (EntityConstants.CheckBoxesModifyConstants[1].equals(button.getText())) {
+                            if (row.getSecondComponent() instanceof JTextField) {
+                                value = ((JTextField) row.getSecondComponent()).getText();
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            } else if (row.getSecondComponent() instanceof JCheckBox) {
+                                value = String.valueOf(((JCheckBox) row.getSecondComponent()).isSelected());
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            } else {
+                                value = ((JComboBox<?>) row.getSecondComponent()).getSelectedItem().toString();
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            }
+                            values.put(key,value);
+                        } else if (EntityConstants.CheckBoxesModifyConstants[0].equals(button.getText())) {
+                            if (row.getSecondComponent() instanceof JTextField) {
+                                value = ((JTextField) row.getSecondComponent()).getText();
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            } else if (row.getSecondComponent() instanceof JCheckBox) {
+                                value = String.valueOf(((JCheckBox) row.getSecondComponent()).isSelected());
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            } else {
+                                value = ((JComboBox<?>) row.getSecondComponent()).getSelectedItem().toString();
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            }
+                            wherevalues.put(key,value);
+                        }
+
+                    }
+                }
+
+            } else if (row.getThirdComponent() instanceof RowPanelOneItem<?>) {
+
+                Enumeration<AbstractButton> buttons = ((RowPanelOneItem<?>) row.getThirdComponent()).getComponentIfRadioButton().getElements();
+                while (buttons.hasMoreElements()) {
+                    JRadioButton button = (JRadioButton) buttons.nextElement();
+                    if (button.isSelected()) {
+                        if (EntityConstants.CheckBoxesModifyConstants[0].equals(button.getText())) {
+                            if (row.getSecondComponent() instanceof JTextField) {
+                                value = ((JTextField) row.getSecondComponent()).getText();
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            } else if (row.getSecondComponent() instanceof JCheckBox) {
+                                value = String.valueOf(((JCheckBox) row.getSecondComponent()).isSelected());
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            } else {
+                                value = ((JComboBox<?>) row.getSecondComponent()).getSelectedItem().toString();
+                                key=((JLabel) row.getFirstComponent()).getText();
+                            }
+                            wherevalues.put(key,value);
+                        }
+
+                    }
+                }
+
+            }
+
+
+        }
+
+        return new LinkedListPair(values, wherevalues);
+    }
+
+
 }
