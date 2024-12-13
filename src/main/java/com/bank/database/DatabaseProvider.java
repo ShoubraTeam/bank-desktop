8c872a617f0a0d5a70a1d39eb426369294342efd
abdalrahman Othman <abdalrahmanOsman1542003@gmail.com>
2024-12-13 15:27:26 +0200

    add ViewEntity class and .env file

--- src/main/java/com/bank/database/DatabaseProvider.java
+++ src/main/java/com/bank/database/DatabaseProvider.java
@@ -24,11 +24,11 @@ public class DatabaseProvider {
         config.setUsername(dbUser);
         config.setPassword(dbPassword);
 
-        config.setMaximumPoolSize(10);
+        config.setMaximumPoolSize(5);  // default 10                    1 source pool enough
         config.setMinimumIdle(2);
-        config.setIdleTimeout(30000); // 30 seconds
-        config.setConnectionTimeout(30000); // 30 seconds
-        config.setMaxLifetime(1800000); // 30 minutes
+        config.setIdleTimeout(60000); // default 30 seconds             value => 1 minute
+        config.setConnectionTimeout(60000); // default 30 seconds       value => 1 minute
+        config.setMaxLifetime(900000); // default 30 minutes        value => 15 minutes
 
         dataSource = new HikariDataSource(config);
     }
@@ -39,3 +39,17 @@ public class DatabaseProvider {
 
     private DatabaseProvider() {}
 }
+// connection time out => time that a client can wait without any connection with database.
+// if the time exceeded the value configured in setConnectionTimeOut() we can throw an exception.
+
+// setIdleTimeout() configure the maximum time that a connection can be idle without any client.
+// setMinimumIdle() configure the minimum idle connection pools
+
+// setMaximumPoolSize() configure the number of pools in the connection pool area
+// example: provided = 4 pools,     used = 2 pools
+// default value is 10 for maximum pool size.
+
+
+// setMaxLifetime() => configure the maximum lifetime of the connection in a pool
+// we can connect to this pool for 30 minutes
+// default value 30 minute
